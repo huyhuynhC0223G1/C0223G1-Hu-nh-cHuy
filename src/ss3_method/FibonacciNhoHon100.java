34b1fcca33dbf4c62da51ead6a6aa42acddabed4
huyhuynhC0223G1 <huynhduchuy403@gmail.com>
2023-04-05 14:02:17 +0700

    em nộp bài tập ngày 5/4

---
+++ src/ss3_method/FibonacciNhoHon100.java
@@ -0,0 +1,25 @@
+package ss3_method;
+
+import java.util.Scanner;
+
+public class FibonacciNhoHon100 {
+    public static void Fibonacci() {
+        int a = 0;
+        int b = 1;
+        int result = 0;
+        int i = 1;
+        while (result < 100) {
+            a = b;
+            b = result;
+            result = a + b;
+            if (result > 100) {
+                break;
+            }
+            System.out.println(result);
+            i++;
+        }
+    }
+    public static void main(String[] args) {
+        Fibonacci();
+    }
+}
