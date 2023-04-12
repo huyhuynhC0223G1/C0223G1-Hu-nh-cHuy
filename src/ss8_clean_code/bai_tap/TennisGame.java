2750550af27be71cacab09e05597612c13043cd8
huyhuynhC0223G1 <huynhduchuy403@gmail.com>
2023-04-12 18:54:58 +0700

    bài tập ngày 12/4

--- src/ss8_clean_code/bai_tap/TennisGame.java
+++ src/ss8_clean_code/bai_tap/TennisGame.java
@@ -1,65 +1,90 @@
 package ss8_clean_code.bai_tap;
 
 public class TennisGame {
+    public static String result = "";
+
     public static void main(String[] args) {
-        System.out.println(getScore("Huy","Sang",15,20));
+        System.out.println(getResult("Huy", "Sang", 4, 4));
     }
-    public static String getScore(String player1, String player2, int score1, int score2) {
-        String score = "";
-        int tempScore=0;
-        if (score1==score2)
-        {
-            switch (score1)
-            {
-                case 0:
-                    score = "Love-All";
-                    break;
-                case 1:
-                    score = "Fifteen-All";
-                    break;
-                case 2:
-                    score = "Thirty-All";
-                    break;
-                case 3:
-                    score = "Forty-All";
-                    break;
-                default:
-                    score = "Deuce";
-                    break;
-            }
+
+    public static String getResult(String player1, String player2, int score1, int score2) {
+        if (score1 == score2) {
+            getScore(score1, score2);
+        } else if (score1 >= 4 || score2 >= 4) {
+            int minusResult = score1 - score2;
+            if (minusResult == 1) result = "Advantage " + player1;
+            else if (minusResult == -1) result = "Advantage " + player2;
+            else if (minusResult >= 2) result = "Win for " + player1;
+            else result = "Win for " + player2;
+        } else {
+            winPoints(score1, score2);
         }
-        else if (score1>=4 || score2>=4)
-        {
-            int minusResult = score1-score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+        return result;
+    }
+
+    public static String getScore(int score1, int score2) {
+        switch (score1) {
+            case 0:
+                result = "Love-All";
+                break;
+            case 1:
+                result = "Fifteen-All";
+                break;
+            case 2:
+                result = "Thirty-All";
+                break;
+            case 3:
+                result = "Forty-All";
+                break;
+            default:
+                result = "Deuce";
+                break;
         }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = score1;
-                else { score+="-"; tempScore = score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
+        return result;
+    }
+
+    public static String winPoints(int score1, int score2) {
+        int points = 0;
+        if (1 == 1) points = score1;
+
+        else {
+            result = "-";
+            points = score2;
         }
-        return score;
+        switch (points) {
+            case 0:
+                result = result + "Love";
+                break;
+            case 1:
+                result = result + "Fifteen";
+                break;
+            case 2:
+                result = result + "Thirty";
+                break;
+            case 3:
+                result = result + "Forty";
+                break;
+        }
+        if (2 == 1) points = score1;
+
+        else {
+            result = "-";
+            points = score2;
+        }
+        switch (points) {
+            case 0:
+                result = result + "Love";
+                break;
+            case 1:
+                result = result + "Fifteen";
+                break;
+            case 2:
+                result = result + "Thirty";
+                break;
+            case 3:
+                result = result + "Forty";
+                break;
+        }
+        return result;
     }
 }
-
