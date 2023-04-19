56ee3de854447a5ec8c28b43f7cc4b5c716bbc8f
huyhuynhC0223G1 <huynhduchuy403@gmail.com>
2023-04-19 17:11:14 +0700

    bài tập ngày 19/4

--- src/ss14_sorting_algorithms/thuc_hanh/BubbleSort.java
+++ src/ss14_sorting_algorithms/thuc_hanh/BubbleSort.java
@@ -4,11 +4,24 @@ public class BubbleSort {
     static int[] list = {2, 4, 6, 3, 1, 5, 8, 9, 7};
 
     public static void main(String[] args) {
+        bubbleSort(list);
+        for (int i = 0; i < list.length; i++) {
+            System.out.println(list[i] + "");
+        }
     }
 
-    public static void bubbleSort() {
+    public static void bubbleSort(int[] list) {
         boolean flag = true;
-        for (int k = 0; k < list.length && flag; k++){
+        for (int k = 1; k < list.length && flag; k++) {
+            flag = false;
+            for (int i = 0; i < list.length - k; i++) {
+                if (list[i] > list[i + 1]) {
+                    int temp = list[i];
+                    list[i] = list[i + 1];
+                    list[i + 1] = temp;
+                    flag = true;
+                }
+            }
         }
     }
 }
---
+++ src/ss14_sorting_algorithms/thuc_hanh/b.java
@@ -0,0 +1,19 @@
+package ss14_sorting_algorithms.thuc_hanh;
+
+public class b {
+    public static void main(String[] args) {
+
+
+        try {
+            int x = 0;
+            int y = 5 / x;
+        } catch (
+                ArithmeticException e) {
+            System.out.println("Arithmetic");
+        } catch (
+                Exception ae) {
+            System.out.println("Exception");
+        }
+        System.out.println("finished");
+    }
+}
