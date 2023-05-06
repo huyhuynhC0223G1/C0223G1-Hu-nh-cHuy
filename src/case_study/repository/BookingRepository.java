8d80b94d7023a77c2120976c801be47c4cc6b937
huyhuynhC0223G1 <huynhduchuy403@gmail.com>
2023-05-06 20:50:59 +0700

    bài tập ngày 05/05

--- src/case_study/repository/BookingRepository.java
+++ src/case_study/repository/BookingRepository.java
@@ -9,9 +9,14 @@ import java.util.Comparator;
 import java.util.TreeSet;
 
 public class BookingRepository implements IBookingRepository {
-    private TreeSet<Booking> bookingList = new TreeSet<>(Comparator.comparing(Booking::getRentalStartDate).
+    private static TreeSet<Booking> bookingList = new TreeSet<>(Comparator.comparing(Booking::getRentalStartDate).
             thenComparing(Booking::getLeaseEndDate));
     private String BOOKING_FILE_PATH = "src/case_study/data/booking.csv";
+    static {
+        bookingList.add(new Booking("BK-1112","11/02/2023","11/03/2023","16/03/2023","KH-1112","SVRO-1112"));
+        bookingList.add(new Booking("BK-1113","01/02/2023","01/03/2023","06/03/2023","KH-1113","SVHO-1113"));
+        bookingList.add(new Booking("BK-1114","21/02/2023","21/03/2023","26/03/2023","KH-1114","SVRO-1114"));
+    }
 
     @Override
     public TreeSet<Booking> showbooking() {
